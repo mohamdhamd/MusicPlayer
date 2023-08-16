c747f788cb8d7573072ede9bdffbd5c54ac3ba4c
Mohamed Hamdy <112737304+mohamdhamd@users.noreply.github.com>
2023-08-16 18:17:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrIcon = document.getElementById("ctrIcon");
+song.onloadedmetadata =()=>{
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+function playPause(){
+    if(ctrIcon.classList.contains("fa-pause"))
+    {
+        song.pause();
+        ctrIcon.classList.remove("fa-pause");
+        ctrIcon.classList.add("fa-play");
+    }else{
+        song.play();
+        ctrIcon.classList.add("fa-pause");
+        ctrIcon.classList.remove("fa-play");
+    }
+}
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value =song.currentTime;
+    },1000)
+}
+
+progress.onchange =()=>{
+    song.play();
+    song.currentTime = progress.value;
+    ctrIcon.classList.add("fa-pause");
+    ctrIcon.classList.remove("fa-play");
+}
